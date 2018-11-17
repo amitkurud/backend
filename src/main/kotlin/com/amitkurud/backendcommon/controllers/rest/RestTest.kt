032beb74d892f2115be36a68eb0ea66d253a8fc4
Amit Kurud <amit.kurud@gmail.com>
2018-11-17 12:10:19 +0530

    validation part2

--- src/main/kotlin/com/amitkurud/backendcommon/controllers/rest/RestTest.kt
+++ src/main/kotlin/com/amitkurud/backendcommon/controllers/rest/RestTest.kt
@@ -3,6 +3,7 @@ package com.amitkurud.backendcommon.controllers.rest
 import com.amitkurud.backendcommon.domain.models.User
 import com.amitkurud.backendcommon.domain.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class RestTest(@Autowired val userRepository : UserRepository){
   @GetMapping("/rest/{username}")
-  fun test(@PathVariable("username") a : String ) : User? {
-    return userRepository.findByUsername(a)
+  fun test(@PathVariable("username") name : String ) : ResponseEntity<User> {
+    return ResponseEntity.ok(userRepository.findByUsername(name)!!)
   }
 }
---
+++ src/main/kotlin/com/amitkurud/backendcommon/controllers/rest/errorhandling/ErrorDetails.kt
@@ -0,0 +1,9 @@
+package com.amitkurud.backendcommon.controllers.rest.errorhandling
+
+import java.util.*
+
+class ErrorDetails(
+        var timeStamp: Date,
+        var message: String,
+        var details: String
+)
